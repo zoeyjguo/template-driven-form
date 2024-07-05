b9298c2380fc2e82b620940ba4b1fed3a1b43b1d
Zoey Guo <zoey-guo@uiowa.edu>
2024-07-05 14:28:58 -0500

    implemented actor form

---
+++ src/app/actor-form/actor-form.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import {Actor} from '../actor';
+
+@Component({
+  selector: 'app-actor-form',
+  templateUrl: './actor-form.component.html',
+})
+export class ActorFormComponent {
+  skills = ['Method Acting', 'Singing', 'Dancing', 'Swordfighting'];
+  model = new Actor(18, 'Tom Cruise', this.skills[3], 'CW Productions');
+  submitted = false;
+  onSubmit() {
+    this.submitted = true;
+  }
+
+  newActor() {
+    this.model = new Actor(42, '', '');
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: false,
 })
-export class AppComponent {
-  title = 'template-driven-form';
-}
+export class AppComponent {}
